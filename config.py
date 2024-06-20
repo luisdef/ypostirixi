9cc2ecada823d4c11865ec549aacf5678910ad93
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2024-06-20 17:36:25 -0300

    Estruturação simples do banco de dados da aplicação

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+DB_HOST = os.environ.get("DATABASE_HOST")
+DB_USER = os.environ.get("DATABASE_USER")
+DB_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+DB_NAME = os.environ.get("DATABASE_NAME")
