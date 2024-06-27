009e69d78274d9a00691e8959d1a7e67c6dec326
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2024-06-27 17:47:09 -0300

    variaveis de ambiente

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
 from flask import Flask
+from flask import request
+
+request.host
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world() -> None:
-    return "<p>Hello, World!</p>"
+    return f"""
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>ypostirixi Server</title>
+        </head>
+        <body>
+            <h1 style='font-family:sans-serif'>
+                Olá mundo!
+            </h1>
+            <p style='font-family:sans-serif'>
+                Rota padrão ativada.
+                Mensagem enviada do servidor do <i>Flask</i> 
+                (<a href='{request.base_url}'>{request.base_url}</a>).
+                Para usar essa aplicação utilize os respectivos
+                <i>end-points</i>.
+            </p>
+        </body>
+        </html>
+    """
+
+
+@app.errorhandler(404)
+def page_not_found(err):
+    print(err)
+    return f"<p style='font-family:sans-serif'>Página não encontrada. <a href=''>Home</a>.</p>"
