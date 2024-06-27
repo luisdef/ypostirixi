85149f0c883f57ee84e4b041f88fc575974bd7dc
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2024-06-27 17:19:55 -0300

    Estrutura inicial client-side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ypostirixiLogo from '/ypostiriki.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="#">
+          <img src={ypostirixiLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>ypostirixi</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Sistema de gerenciamento de Suporte de TI.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Modelo de tela inicial.
       </p>
     </>
   )
 }
-
-export default App
