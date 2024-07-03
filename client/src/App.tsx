586c53062c6f4896706433712ae413bae45192a3
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2024-07-03 01:07:32 -0300

    frontend home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,10 @@
-import ypostirixiLogo from '/ypostiriki.svg'
-import './App.css'
+import Home from "./pages/home";
 
 export default function App() {
   return (
-    <>
-      <div>
-        <a href="#">
-          <img src={ypostirixiLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>ypostirixi</h1>
-      <div className="card">
-        <p>
-          Sistema de gerenciamento de Suporte de TI.
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Modelo de tela inicial.
-      </p>
-    </>
+    <div>
+        <div id="back-container"></div>
+        <Home />
+    </div>
   )
 }
---
+++ client/src/pages/home.tsx
@@ -0,0 +1,38 @@
+import ypostirixiLogo from '/ypostiriki.svg'
+
+export default function Home() {
+  return (
+    <div className='flex justify-center items-center h-screen max-md:flex-col'>
+        <div className='flex-col justify-center items-center'>
+            <div className='flex justify-center items-center py-8'>
+                <a href="" className=''>
+                    <img src={ypostirixiLogo} className="logo w-[150px]" alt="Vite logo" />
+                </a>
+            </div>
+            <h1 className='text-4xl font-bold w-full text-center'>ypostirixi</h1>
+            <div className="card w-full text-center py-8">
+                <p>
+                    Sistema de gerenciamento de Suporte de TI.
+                </p>
+            </div>
+        </div>
+
+        <div className='pl-4 flex-col justify-center items-center'>
+            <div className='border-2 border-[#241f34] rounded-md p-2 my-4 cursor-pointer select-none hover:bg-white'>
+                <p className='font-bold'>Abrir OS</p>
+                <span>Abrir um novo chamado.</span>
+            </div>
+
+            <div className='border-2 border-[#241f34] rounded-md p-2 my-4 cursor-pointer select-none hover:bg-white'>
+                <p className='font-bold'>Abrir OS</p>
+                <span>OSs abertas e em andamento.</span>
+            </div>
+
+            <div className='border-2 border-[#241f34] rounded-md p-2 my-4 cursor-pointer select-none hover:bg-white'>
+                <p className='font-bold'>Dashboard OS</p>
+                <span>Painel de controle de OSs.</span>
+            </div>
+        </div>
+    </div>
+  )
+}
