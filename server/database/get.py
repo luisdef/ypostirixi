13d7406ba89877f57b5c442b850d4552d4a03123
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2024-07-25 03:26:31 -0300

    database changes

--- server/database/get.py
+++ server/database/get.py
@@ -1,11 +1,12 @@
 from config import DB_HOST, DB_NAME, DB_USER, DB_PASSWORD
-from sqlalchemy import create_engine, text
+from sqlalchemy import text
+
+from server.database.generate import connect_engine
 
 def select_user_by_email(email: str):
     result = []
     try:
-        engine = create_engine(f'mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}',
-                           echo=True)
+        engine = connect_engine()
         with engine.connect() as conn:
             query = text(f'SELECT * FROM users WHERE email = \'{email}\'')
             result = conn.execute(query).fetchone()
@@ -17,6 +18,67 @@ def select_user_by_email(email: str):
                 'role': result[4]
             }
     except Exception as e:
-        print('\nSome error with the database, check connection.\n')
+        print('\n[While select] Some error with the database, check connection.\n')
         print(repr(e), '\n')
         return False
+
+
+def select_all_oss():
+    try:
+        engine = connect_engine()
+        with engine.connect() as conn:
+            query = text(f'select * from orders order by updated_at desc')
+            results = conn.execute(query).fetchall()
+            
+            oss = []
+            for res in results:
+                os = {
+                    "uuid": res[0],
+                    "nome": res[1],
+                    "email": res[2],
+                    "fone": res[3],
+                    "setor": res[4],
+                    "problema": res[5],
+                    "descricao": res[6],
+                    "prioridade": res[7],
+                    "descricaoTec": str(res[8]) if res[8] else '',
+                    "status": res[9],
+                    "created_at": res[10].strftime("%Y/%m/%d %H:%m"),
+                    "updated_at": res[11].strftime("%Y/%m/%d %H:%m")
+                }
+                oss.append(os)
+            
+            return oss
+    except Exception as e:
+        print('\n[While select] Some error with the database, check connection.\n')
+        print(repr(e), '\n')
+        return False
+
+
+def select_os_by_uuid(uuid: str):
+    result = []
+    try:
+        engine = connect_engine()
+        with engine.connect() as conn:
+            query = text(f'select * from orders where uuid = \'{uuid}\'')
+            result = conn.execute(query).fetchone()
+
+            return {
+                'uuid': result[0],
+                'nome': result[1],
+                'email': result[2],
+                'fone': result[3],
+                'setor': result[4],
+                'problema': result[5],
+                'descricao': result[6],
+                'prioridade': result[7],
+                'descricaoTec': str(result[8]) if result[8] else '',
+                'status': result[9],
+                'created_at': result[10].strftime("%Y/%m/%d %H:%m"),
+                'updated_at': result[11].strftime("%Y/%m/%d %H:%m")
+            }
+    except Exception as e:
+        print('\n[While select] Some error with the database, check connection.\n')
+        print(repr(e), '\n')
+        return False
+
--- server/database/ypos.sql
+++ server/database/ypos.sql
@@ -1,25 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Tempo de geração: 25/07/2024 às 06:03
+-- Versão do servidor: 8.0.36
+-- Versão do PHP: 7.4.33
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Banco de dados: `ypos`
+--
 CREATE DATABASE IF NOT EXISTS `ypos` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE `ypos`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `orders`
+--
 
 CREATE TABLE IF NOT EXISTS `orders` (
   `uuid` varchar(36) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `phone` varchar(30) NOT NULL,
-  `sector` int NOT NULL,
+  `sector` varchar(255) NOT NULL,
   `problem` varchar(255) NOT NULL,
   `description` varchar(1023) NOT NULL,
-  `priority` int NOT NULL DEFAULT '1',
-  `technician_report` varchar(1023) NOT NULL,
+  `priority` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `technician_report` varchar(1023) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `status` int NOT NULL DEFAULT '1',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -29,15 +48,11 @@ CREATE TABLE IF NOT EXISTS `orders` (
   KEY `status` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS `priority` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `priority` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- --------------------------------------------------------
 
-INSERT INTO `priority` (`id`, `priority`) VALUES
-(1, 'Normal'),
-(2, 'Urgente');
+--
+-- Estrutura para tabela `roles`
+--
 
 CREATE TABLE IF NOT EXISTS `roles` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -45,21 +60,19 @@ CREATE TABLE IF NOT EXISTS `roles` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Despejando dados para a tabela `roles`
+--
+
 INSERT INTO `roles` (`id`, `role`) VALUES
 (1, 'Admin'),
 (2, 'Client');
 
-CREATE TABLE IF NOT EXISTS `sectors` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `sector` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- --------------------------------------------------------
 
-INSERT INTO `sectors` (`id`, `sector`) VALUES
-(1, 'Administração'),
-(2, 'Contabilidade'),
-(3, 'Marketing'),
-(4, 'Vendas');
+--
+-- Estrutura para tabela `status`
+--
 
 CREATE TABLE IF NOT EXISTS `status` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -67,11 +80,21 @@ CREATE TABLE IF NOT EXISTS `status` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Despejando dados para a tabela `status`
+--
+
 INSERT INTO `status` (`id`, `status`) VALUES
 (1, 'Aberta'),
 (2, 'Em Serviço'),
 (3, 'Pendente'),
 (4, 'Encerrada');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `users`
+--
 
 CREATE TABLE IF NOT EXISTS `users` (
   `uuid` varchar(36) NOT NULL,
@@ -83,15 +106,26 @@ CREATE TABLE IF NOT EXISTS `users` (
   KEY `role` (`role`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Despejando dados para a tabela `users`
+--
+
 INSERT INTO `users` (`uuid`, `name`, `email`, `password`, `role`) VALUES
 ('4dfbbcbd-38d3-11ef-a4c2-56f540a536a8', 'Master', 'master@admin.com', '$2y$10$KRLRIDQdVkjvHEUuVL1R5.cZv0C8fdTp45wNjFP94fI2aLhRLaQqC', 1);
 
+--
+-- Restrições para tabelas despejadas
+--
 
+--
+-- Restrições para tabelas `orders`
+--
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`sector`) REFERENCES `sectors` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`priority`) REFERENCES `priority` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
   ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`status`) REFERENCES `status` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
+--
+-- Restrições para tabelas `users`
+--
 ALTER TABLE `users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role`) REFERENCES `roles` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 COMMIT;
