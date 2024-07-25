876b14e12d2bb71d044ac4533f6c824a024e7484
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2024-07-25 03:27:55 -0300

    tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,8 @@ export default {
     content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
+      "./src/**/**/*.{js,ts,jsx,tsx}",
+      "./src/components/*.tsx",
     ],
     theme: {
       extend: {},
