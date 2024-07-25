3bc580d61b102fcfa173b9f48c34e68817518ca8
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2024-07-25 03:29:01 -0300

    structure and design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Login from './pages/login'
 import List from './pages/list'
+import Consult from './pages/consult'
+import New from './pages/new'
+import DashboardList from './pages/dashboardList'
+import DashboardConsult from './pages/dashboardConsult'
 
 const App: React.FC = () => {
   return (
@@ -9,8 +13,12 @@ const App: React.FC = () => {
       <div id='back-container'></div>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/new' element={<New />} />
         <Route path='/list' element={<List />} />
+        <Route path='/os' element={<Consult />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/maintenance/list' element={<DashboardList />} />
+        <Route path='/maintenance/os' element={<DashboardConsult />} />
       </Routes>
     </Router>
   )
