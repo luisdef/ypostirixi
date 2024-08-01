029f5f52b7332fd49962ae9e65b9fa63b9567814
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2024-08-01 00:09:39 -0300

    my mark

---
+++ client/src/components/mark.tsx
@@ -0,0 +1,7 @@
+export default function MarkId() {
+  return (
+    <div className='fixed bottom-2 w-full flex justify-center items-center select-none'>
+      <p className='opacity-65'>Desenvolvido por <strong>Luis Felipe Assmann</strong>.</p>
+    </div>
+  )
+}
